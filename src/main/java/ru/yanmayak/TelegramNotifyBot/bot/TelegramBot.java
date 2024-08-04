ce6ae9bd6e2bf132c6adc24475c291e1091797ac
Marina Dvoretskova <smallestproblem@gmail.com>
2024-08-04 14:35:50 +0300

    Added dependency for telegram bot. Changed Spring Boot version to 2.7.6 to resolve conflict with telegrambots-spring-boot-starter. Added stub (echo) bot.

---
+++ src/main/java/ru/yanmayak/TelegramNotifyBot/bot/TelegramBot.java
@@ -0,0 +1,45 @@
+package ru.yanmayak.TelegramNotifyBot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Value("${bot.username}")
+    private String botUsername;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage message = new SendMessage(chatId, text);
+
+            try {
+                execute(message);
+            }
+            catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
